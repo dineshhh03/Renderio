c1976cab25dfe6be63c0e18d2318731b76ac51ed
Dinesh <dineshps1903@gmail.com>
2024-10-16 20:08:45 +0530

    adding home page and create post

---
+++ src/components/FormField.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FormField = ({
+  LableName,
+  type,
+  name,
+  placeholder,
+  value,
+  isSurpriseMe,
+  handleChange,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label></label>
+      </div>
+    </div>
+  );
+};
+
+export default FormField;
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+  const handleSurpriseMe = () => {};
+  return (
+    <section className="max-w-[7xl] mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Create imaginative and visually stunning images through Renderio and
+          share them in community.
+        </p>
+      </div>
+      <form className="mt-16 max-w-[3xl]" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            LabelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            LabelName="Prompt name"
+            type="text"
+            name="prompt"
+            placeholder="teddy bears shopping for groceries in Japan, ukiyo-e"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
